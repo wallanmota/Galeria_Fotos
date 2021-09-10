dd5a0fd87c11a283085a9693e6140988080f0d8f
wallanmota <wallanmota.13@gmail.com>
2021-09-10 10:25:45 -0300

    logica click alterada

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 let fullImgBox =  document.getElementById("fullImgBox");
 let fullImg = document.getElementById("fullImg");
 
-let img1 = document.getElementById("img1").addEventListener("click", openImg(this.src));
-let img2 = document.getElementById("img2");
-let img3 = document.getElementById("img3");
-let img4 = document.getElementById("img4");
-let img5 = document.getElementById("img5");
-let img6 = document.getElementById("img6");
-let img7 = document.getElementById("img7");
-let img8 = document.getElementById("img8");
+let img1 = document.getElementById("img1").addEventListener('click', openImg);
+let img2 = document.getElementById("img2").addEventListener('click', openImg);
+let img3 = document.getElementById("img3").addEventListener('click', openImg);
+let img4 = document.getElementById("img4").addEventListener('click', openImg);
+let img5 = document.getElementById("img5").addEventListener('click', openImg);
+let img6 = document.getElementById("img6").addEventListener('click', openImg);
+let img7 = document.getElementById("img7").addEventListener('click', openImg);
+let img8 = document.getElementById("img8").addEventListener('click', openImg);
 
-img2.onclick = openImg(this.src)
 
 let fechar = document.getElementById("fechar").addEventListener('click', closeImg)
 
-function openImg(pic){
+function openImg(){
     fullImgBox.style.display = "flex";
-    fullImg.src = pic;
+    fullImg.src = this.src;
+    console.log('clicou')
 }
 
 function closeImg(){
     fullImgBox.style.display = "none";
+    console.log('fechou')
+    
 }
 
