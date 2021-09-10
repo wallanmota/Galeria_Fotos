62574e46e8217a97b39b1e6305f8f29e3ed4f7f1
wallanmota <wallanmota.13@gmail.com>
2021-09-09 21:50:35 -0300

    em teste

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 let fullImgBox =  document.getElementById("fullImgBox");
 let fullImg = document.getElementById("fullImg");
 
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let img3 = document.getElementById("img3");
+let img4 = document.getElementById("img4");
+let img5 = document.getElementById("img5");
+let img6 = document.getElementById("img6");
+let img7 = document.getElementById("img7");
+let img8 = document.getElementById("img8");
 
+img1.addEventListener('click(this.src)', openImg)
 
 function openImg(pic){
     fullImgBox.style.display = "flex";
